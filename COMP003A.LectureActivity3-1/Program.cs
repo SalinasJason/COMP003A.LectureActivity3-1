9867659eee50d666920a7726b6e2351e70b7e095
SalinasJason <jasonsalinas770@gmail.com>
2025-01-27 20:02:22 -0800

    Added comments at the top of the program

--- COMP003A.LectureActivity3-1/Program.cs
+++ COMP003A.LectureActivity3-1/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace COMP003A.LectureActivity3_1
+﻿/*
+ Author: Jason Salinas
+ Course: COMP-003A
+ Faculty: Jonathan Cruz
+ Purpose: Demonstrate selection statements in C#
+*/
+namespace COMP003A.LectureActivity3_1
 {
     internal class Program
     {
